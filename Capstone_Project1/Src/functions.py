dbf3a8c86d9bd0489d46968f9d35d8ebc65a8901
Shiva Ramakrishnan <112168234+ShivaSRK2002@users.noreply.github.com>
2024-07-11 13:57:36 +0530

    Codes Updated according to comments given

--- Capstone_Project1/Src/functions.py
+++ Capstone_Project1/Src/functions.py
@@ -1,179 +1,83 @@
-import os
-import time
-import pytest
-from selenium import webdriver
-from selenium.common import TimeoutException
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
-from Src.locators import LoginLocators as lc
+from selenium.webdriver.support.ui import WebDriverWait
 
 
-class testmethods:
-    @pytest.fixture
-    def browser(self):
-        # Path to the ChromeDriver executable
-        chromedriver_path = r"C:\Users\shiva\OneDrive\Desktop\chromedriver.exe"
-        if not os.path.exists(chromedriver_path):
-            raise FileNotFoundError(f"ChromeDriver not found at path: {chromedriver_path}")
-        os.environ["PATH"] += os.pathsep + os.path.dirname(chromedriver_path)
-
-        # Set Chrome options
-        chrome_options = Options()
-        chrome_options.add_experimental_option("detach", True)
-
-        # Initialize the WebDriver service
-        service = Service(executable_path=chromedriver_path)
-        driver = webdriver.Chrome(service=service, options=chrome_options)
-
-        # Provide the driver to the test and quit after the test is done
-        yield driver
-        time.sleep(6)
-        driver.quit()
-
+class OrangeHRM:
     @staticmethod
-    def login_to_OrangeHRM(driver, username, password):
-        # Wait for the username field to be present and enter the username
+    def login(driver, username, password, locators):
         WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, lc.username_element))).send_keys(username)
-
-        # Wait for the password field to be present and enter the password
+            EC.presence_of_element_located((By.XPATH, locators.USERNAME_INPUT))).send_keys(username)
         WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, lc.password_element))).send_keys(password)
-
-        # Wait for the submit button to be present and click it
+            EC.presence_of_element_located((By.XPATH, locators.PASSWORD_INPUT))).send_keys(password)
         WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, lc.submit_element))).click()
-
+            EC.presence_of_element_located((By.XPATH, locators.SUBMIT_BUTTON))).click()
         try:
-            # Wait for the dashboard URL to indicate successful login
             WebDriverWait(driver, 10).until(EC.url_contains("dashboard"))
-
-            # Wait for the profile button and click it
             WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, lc.profile_button_element))).click()
-
-            # Wait for the logout button and click it
+                EC.presence_of_element_located((By.XPATH, locators.PROFILE_BUTTON))).click()
             WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, lc.logout_button_element))).click()
-
+                EC.presence_of_element_located((By.XPATH, locators.LOGOUT_BUTTON))).click()
             return "The user has logged in successfully"
         except TimeoutException:
             return "Invalid credentials"
 
     @staticmethod
-    def add_new_employee(driver):
-        # Click the PIM module
-        pim_module = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, lc.pim)))
-        pim_module.click()
-
-        # Click the add employee button
-        add_employee = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, lc.add_employee)))
-        add_employee.click()
-
-        # Enter first name and last name
-        first_name = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, lc.fir_nam)))
-        last_name = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, lc.las_nam)))
-        first_name.send_keys("Shiva")
-        last_name.send_keys("RK")
-
-        # Click the save button
-        save_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, lc.save)))
-        save_button.click()
-
+    def add_employee(driver, locators):
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, locators.PIM_MODULE))).click()
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, locators.ADD_EMPLOYEE_BUTTON))).click()
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, locators.FIRST_NAME_INPUT))).send_keys("Shiva")
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, locators.LAST_NAME_INPUT))).send_keys("RK")
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, locators.SAVE_BUTTON))).click()
         try:
-            # Wait for the success message and return its text
-            toaster = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, lc.saved_success)))
-            value = toaster.text
-            return value
+            success_message = WebDriverWait(driver, 15).until(
+                EC.presence_of_element_located((By.XPATH, locators.SUCCESS_MESSAGE)))
+            return success_message.text
         except TimeoutException:
-            value = "Unable to save"
-            return value
+            return "Unable to save"
 
     @staticmethod
-    def edit_employee_details(driver):
-        # Click the PIM module
-        pim_module = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, lc.pim)))
-        pim_module.click()
-
-        # Click the employee list
-        employee_list = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, lc.employee_list)))
-        employee_list.click()
-
-        # Select the user to edit and click the edit button
-        select_user = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, lc.sel_user_edit)))
-        select_user.click()
-        edit_user = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, lc.click_edit)))
-        edit_user.click()
-
-        # Click the male gender button
-        male_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, lc.male_button)))
-        male_button.click()
-
-        # Click the submit button to save changes
-        submit_edited_user = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, lc.submit_edit_user)))
-        submit_edited_user.click()
-
+    def edit_employee(driver, locators):
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, locators.PIM_MODULE))).click()
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, locators.EMPLOYEE_LIST_BUTTON))).click()
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, locators.SELECT_USER_EDIT))).click()
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, locators.EDIT_BUTTON))).click()
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, locators.MALE_BUTTON))).click()
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, locators.SUBMIT_EDIT_BUTTON))).click()
         try:
-            # Wait for the success message and return its text
-            toaster = WebDriverWait(driver, 15).until(
-                EC.presence_of_element_located((By.XPATH, lc.update_success)))
-            value = toaster.text
-            return value
+            success_message = WebDriverWait(driver, 15).until(
+                EC.presence_of_element_located((By.XPATH, locators.UPDATE_SUCCESS_MESSAGE)))
+            return success_message.text
         except TimeoutException:
-            value = "Unable to update user details"
-            return value
+            return "Unable to update user details"
 
     @staticmethod
-    def login_to_OrangeHRM_stat(driver, username, password):
-        # Wait for the username field to be present and enter the username
+    def delete_employee(driver, locators):
         WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, lc.username_element))).send_keys(username)
-
-        # Wait for the password field to be present and enter the password
+            EC.presence_of_element_located((By.XPATH, locators.PIM_MODULE))).click()
         WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, lc.password_element))).send_keys(password)
-
-        # Wait for the submit button to be present and click it
+            EC.presence_of_element_located((By.XPATH, locators.EMPLOYEE_LIST_BUTTON))).click()
         WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, lc.submit_element))).click()
-
+            EC.presence_of_element_located((By.XPATH, locators.SELECT_USER_DELETE))).click()
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, locators.DELETE_BUTTON))).click()
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, locators.CONFIRM_DELETE_BUTTON))).click()
         try:
-            # Wait for the dashboard URL to indicate successful login
-            WebDriverWait(driver, 10).until(EC.url_contains("dashboard"))
-            return "The user has logged in successfully"
+            success_message = WebDriverWait(driver, 15).until(
+                EC.presence_of_element_located((By.XPATH, locators.DELETE_SUCCESS_MESSAGE)))
+            return success_message.text
         except TimeoutException:
-            return "Invalid credentials"
-
-    @staticmethod
-    def delete_employee(driver):
-        # Click the PIM module
-        pim_module = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, lc.pim)))
-        pim_module.click()
-
-        # Click the employee list
-        employee_list = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, lc.employee_list)))
-        employee_list.click()
-
-        # Select the user to delete and click the delete button
-        select_user_del = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, lc.sel_user_del)))
-        select_user_del.click()
-        delete_user = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, lc.delete_user)))
-        delete_user.click()
-
-        # Confirm the deletion
-        confirm_delete = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, lc.confirm_del)))
-        confirm_delete.click()
-
-        try:
-            # Wait for the success message and return its text
-            toaster = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, lc.del_success)))
-            value = toaster.text
-            return value
-        except TimeoutException:
-            value = "Unable to delete the selected user"
-            return value
+            return "Unable to delete the selected user"
--- Capstone_Project1/Test_LoginPage/test_login.py
+++ Capstone_Project1/Test_LoginPage/test_login.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import time
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -10,105 +9,68 @@ from selenium.webdriver.chrome.service import Service
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # Import the necessary modules and classes from the project
-from Src.functions import testmethods as tm
-from Src.locators import LoginLocators as vl
-import Src.locators as lc
-from Capstone_Project1.Src.locators import LoginLocators as vl
+# from Src.functions import OrangeHRM
+# from Src.locators import LoginPageLocators, PIMPageLocators
+from Capstone_Project1.Src.functions import OrangeHRM
+from Capstone_Project1.Src.locators import LoginPageLocators, PIMPageLocators
 
 
 @pytest.fixture(scope="class")
 def setup(request):
-    # Path to the ChromeDriver executable
     chromedriver_path = r"C:\Users\shiva\OneDrive\Desktop\chromedriver.exe"
     if not os.path.exists(chromedriver_path):
         raise FileNotFoundError(f"ChromeDriver not found at path: {chromedriver_path}")
 
-    # Add the directory containing ChromeDriver to the system path
     os.environ["PATH"] += os.pathsep + os.path.dirname(chromedriver_path)
 
-    # Set Chrome options
     chrome_options = Options()
     chrome_options.add_experimental_option("detach", True)
 
-    # Initialize the WebDriver service
     service = Service(executable_path=chromedriver_path)
     driver = webdriver.Chrome(service=service, options=chrome_options)
-
-    # Maximize the browser window
     driver.maximize_window()
 
-    # Attach the WebDriver instance to the test class
     request.cls.driver = driver
-
-    # Provide the driver to the test and quit after the test is done
     yield
-    time.sleep(6)
     driver.quit()
 
 
 @pytest.mark.usefixtures("setup")
-class TestLoginPage:
+class TestOrangeHRM:
 
-    def test_TC_login_01(self):
-        # Navigate to the login URL
-        self.driver.get(lc.login_url)
-
-        # Perform login with valid credentials
-        result = tm.login_to_OrangeHRM(self.driver, vl.valid_username, vl.valid_password)
-
-        # Assert that login was successful
+    def test_login_valid(self):
+        self.driver.get(LoginPageLocators.LOGIN_URL)
+        result = OrangeHRM.login(self.driver, LoginPageLocators.VALID_USERNAME, LoginPageLocators.VALID_PASSWORD,
+                                 LoginPageLocators)
         assert result == "The user has logged in successfully"
 
-    def test_TC_login_02(self):
-        # Navigate to the login URL
-        self.driver.get(lc.login_url)
-
-        # Perform login with invalid credentials
-        result = tm.login_to_OrangeHRM(self.driver, vl.valid_username, vl.invalid_password)
-
-        # Assert that login failed
+    def test_login_invalid(self):
+        self.driver.get(LoginPageLocators.LOGIN_URL)
+        result = OrangeHRM.login(self.driver, LoginPageLocators.INVALID_USERNAME, LoginPageLocators.INVALID_PASSWORD,
+                                 LoginPageLocators)
         assert result == "Invalid credentials"
 
-    def test_TC_PIM_01(self):
-        # Navigate to the login URL
-        self.driver.get(lc.login_url)
-
-        # Perform login with valid credentials
-        tm.login_to_OrangeHRM_stat(self.driver, vl.valid_username, vl.valid_password)
-
-        # Add a new employee
-        result = tm.add_new_employee(self.driver)
-
-        # Assert that the new employee was added successfully
+    def test_add_employee(self):
+        self.driver.get(LoginPageLocators.LOGIN_URL)
+        OrangeHRM.login(self.driver, LoginPageLocators.VALID_USERNAME, LoginPageLocators.VALID_PASSWORD,
+                        LoginPageLocators)
+        result = OrangeHRM.add_employee(self.driver, PIMPageLocators)
         assert result == "Successfully Saved"
 
-    def test_TC_PIM_02(self):
-        # Navigate to the login URL
-        self.driver.get(lc.login_url)
-
-        # Perform login with valid credentials (commented out if not needed again)
-        # tm.login_to_OrangeHRM_stat(self.driver, vl.valid_username, vl.valid_password)
-
-        # Edit employee details
-        result = tm.edit_employee_details(self.driver)
-
-        # Assert that the employee details were updated successfully
+    def test_edit_employee(self):
+        self.driver.get(LoginPageLocators.LOGIN_URL)
+        OrangeHRM.login(self.driver, LoginPageLocators.VALID_USERNAME, LoginPageLocators.VALID_PASSWORD,
+                        LoginPageLocators)
+        result = OrangeHRM.edit_employee(self.driver, PIMPageLocators)
         assert result == "Successfully Updated"
 
-    def test_TC_PIM_03(self):
-        # Navigate to the login URL
-        self.driver.get(lc.login_url)
-
-        # Perform login with valid credentials (commented out if not needed again)
-        # tm.login_to_OrangeHRM_stat(self.driver, vl.valid_username, vl.valid_password)
-
-        # Delete an employee
-        result = tm.delete_employee(self.driver)
-
-        # Assert that the employee was deleted successfully
+    def test_delete_employee(self):
+        self.driver.get(LoginPageLocators.LOGIN_URL)
+        OrangeHRM.login(self.driver, LoginPageLocators.VALID_USERNAME, LoginPageLocators.VALID_PASSWORD,
+                        LoginPageLocators)
+        result = OrangeHRM.delete_employee(self.driver, PIMPageLocators)
         assert result == "Successfully Deleted"
 
 
 if __name__ == "__main__":
-    # Run the tests
     pytest.main(["-v", "test_login.py"])
